c943a3d331c07e19ba93a9b7233002597cc294e9
pepermz <74684215+pepermz@users.noreply.github.com>
2021-04-04 18:56:14 -0500

    merge

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 //const sequelize = require('./config/connection');
 const path = require('path');
 const express = require('express');
+const bodyParser= require('body-parser');
 const app = express();
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 const routes = require('./controllers'); 
 
 const exphbs = require('express-handlebars');
@@ -19,13 +21,26 @@ app.set('view engine', 'handlebars');
 // routing to main page, login page, calendar page, profile page
 app.get('/', (req, res) => {
     res.render('home', { 
-        title: 'Recess' 
+        title: 'Recess II' 
     });
 });
 app.get('/login', (req, res) => {
     res.render('login', { 
         title: 'User Login' 
     });
+})
+app.post('/login', (req, res) => {
+   console.log(req.body);
+   const pw = req.body.password;
+   if (pw!='test1234')
+   {
+       res.end("Invalid login");
+   }
+   else {
+       res.redirect("/");
+   }
+   //res.json(req.body);
+   res.end();
 })
 app.get('/about', (req, res) => {
     res.render('about', { 
