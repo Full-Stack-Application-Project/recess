a67ac2091ec7da703f207215ec7c5b82e41ae05f
Nate Schroeder <n8dogg59@hotmail.com>
2021-04-05 20:18:50 -0500

    sign-up issue fixed

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -22,39 +22,8 @@ async function loginFormHandler(event) {
   }
 }
 
-async function signupFormHandler(event) {
-  event.preventDefault();
-  const firstname = document.querySelector("#first-name").value.trim();
-  const lastname = document.querySelector("#last-name").value.trim();
-  const email = document.querySelector("#email-signup").value.trim();
-  const password = document.querySelector("#password-signup").value.trim();
 
-  if (firstname && lastname && email && password) {
-    const response = await fetch("/api/users", {
-      method: "post",
-      body: JSON.stringify({
-        firstname,
-        lastname,
-        email,
-        password
-      }),
-      headers: { "Content-Type": "application/json" },
-    });
-
-    console.log("user posted");
-
-    if (response.ok) {
-      document.location.replace("/dashboard");
-    } else {
-      alert(response.statusText);
-    }
-  }
-}
 
 document
   .querySelector(".login-form")
   .addEventListener("submit", loginFormHandler);
-
-document
-  .querySelector(".signup-form")
-  .addEventListener("submit", signupFormHandler);
