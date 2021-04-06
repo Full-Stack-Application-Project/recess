ad587901354626c92fc58f920feadcb00c1943c6
Cnidarian <cmphillips23@gmail.com>
2021-04-06 17:57:09 -0500

    fixing merges

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-//const sequelize = require('./config/connection');
 const path = require('path');
 const express = require('express');
-// const session = require('express-session');
 const routes = require('./controllers');
-// const session = require('express-session');
-// const exphbs = require('express-handlebars');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
 
 const sequelize = require('./config/connection');
 const app = express();
 
-const exphbs = require('express-handlebars');
 
 app.set ("view engine", "handlebars");
 app.engine('handlebars',exphbs({
@@ -20,21 +17,30 @@ app.engine('handlebars',exphbs({
 }));
 const PORT = process.env.PORT || 3006;
 
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
-/* 
-const routes = require('./controllers'); 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const exphbs = require('express-handlebars');
-//const hbs = exphbs.create({});
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
-const PORT = process.env.PORT || 3001;
+app.use(session(sess));
 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
-app.listen(PORT, () => console.log('Now listening: '+PORT)); */
-/*
+app.listen(PORT, () => console.log('Now listening: '+PORT));
+
 sequelize.sync({ force: false }).then(() => {
   
 });
-*/
