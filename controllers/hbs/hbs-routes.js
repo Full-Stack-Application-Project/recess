397c444fd0eb09b271b4c40957c40082b1a629a6
pepermz <74684215+pepermz@users.noreply.github.com>
2021-04-06 01:44:55 -0500

    added scss to categories

--- controllers/hbs/hbs-routes.js
+++ controllers/hbs/hbs-routes.js
@@ -13,10 +13,34 @@ router.get("/signup", (req, res)=>{
 router.get("/dashboard", (req, res)=>{
     res.render("dashboard", {title: "Dashboard"});
 });
-// router.get("/activity", (req, res)=>{
-//     res.render("activity", {title: "Activities"});
-// });
-// router.get("/about", (req, res)=>{
-//     res.render("about", {title: "About Us"});
-// });
+ router.get("/activity", (req, res)=>{
+    res.render("activity", {title: "Activities"});
+});
+ router.get("/about", (req, res)=>{
+    // console.log ("about");
+    res.render("about", {title: "About Us"});
+});
+router.get("/categories", (req, res)=>{
+    res.render("categories", {title: "categories"});
+});
+router.get("/form1/", (req, res)=>{
+    //const formNum = parseInt(req.params.fn) ;
+    const formNum = req.params.fn;
+    const forms=['form1', 'form2'];
+   // console.log(forms[formNum]);
+ //  const form = forms[formNum]+"";
+    switch (formNum){
+        case '1':
+            res.render("form1", {title:"Form "});
+            break;
+        case '2':
+            res.render("form2", {title:"Form "});
+            break;
+    }
+    
+  //  res.write ("form: "+formNum);
+ 
+    //res.end();
+   // res.render("categories", {title: "categories"});
+});
 module.exports = router;
