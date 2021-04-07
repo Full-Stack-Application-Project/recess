b8947f5d9a00da6b5732db37d470e2ae528dbafd
Nate Schroeder <n8dogg59@hotmail.com>
2021-04-07 18:55:24 -0500

    updated logout.js

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -1,35 +1,34 @@
-// function getUserId(event) {
-//   event.preventDefault();
-//   fetch("/api/users/loggedIn").then(function (response) {
-//     return response.json().then(function (response) {
-//       let id = response[0].id;
-//       console.log(id);
-//       editUser(id);
-//     });
-//   });
-// }
-
-// async function editUser(id) {
-//   console.log(id);
-//   const response = await fetch(`/api/users/${id}`, {
-//     method: 'PATCH',
-//     body: JSON.stringify({
-//       loggedIn: "false"
-//     }),
-//     headers: {
-//       'Content-Type': 'application/json'
-//     }
-//   });
-
-//   if (response.ok) {
-//     logout();
-//   } else {
-//     alert(response.statusText);
-//   }
-// }
-
-async function logout(event) {
+function getUserId(event) {
   event.preventDefault();
+  fetch("/api/users/loggedIn").then(function (response) {
+    return response.json().then(function (response) {
+      let id = response[0].id;
+      console.log(id);
+      editUser(id);
+    });
+  });
+}
+
+async function editUser(id) {
+  console.log(id);
+  const response = await fetch(`/api/users/${id}`, {
+    method: 'PATCH',
+    body: JSON.stringify({
+      loggedIn: "false"
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  if (response.ok) {
+    logout();
+  } else {
+    alert(response.statusText);
+  }
+}
+
+async function logout() {
   const response = await fetch("/api/users/logout", {
     method: "post",
     headers: { "Content-Type": "application/json" },
@@ -42,4 +41,4 @@ async function logout(event) {
   }
 }
 
-document.querySelector("#logout").addEventListener("click", logout);
+document.querySelector("#logout").addEventListener("click", getUserId);
