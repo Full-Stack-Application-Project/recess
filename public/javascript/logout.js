58fa79f705cc83baf403dd6420c514fa2991772b
Nate Schroeder <n8dogg59@hotmail.com>
2021-04-07 17:35:04 -0500

    forgot to save

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -1,33 +1,33 @@
-function getUserId(event) {
-  event.preventDefault();
-  fetch("/api/users/loggedIn").then(function (response) {
-    return response.json().then(function (response) {
-      console.log(response);
-      console.log("got to presetschedule.js line35");
-      let id = response[0].id;
-      console.log(id);
-      editUser(id);
-    });
-  });
-}
+// function getUserId(event) {
+//   event.preventDefault();
+//   fetch("/api/users/loggedIn").then(function (response) {
+//     return response.json().then(function (response) {
+//       console.log(response);
+//       console.log("got to presetschedule.js line35");
+//       let id = response[0].id;
+//       console.log(id);
+//       editUser(id);
+//     });
+//   });
+// }
 
-async function editUser(id) {
-  const response = await fetch(`/api/users/${id}`, {
-    method: 'PUT',
-    body: JSON.stringify({
-      id 
-    }),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  });
+// async function editUser(id) {
+//   const response = await fetch(`/api/users/${id}`, {
+//     method: 'PUT',
+//     body: JSON.stringify({
+//       id 
+//     }),
+//     headers: {
+//       'Content-Type': 'application/json'
+//     }
+//   });
 
-  if (response.ok) {
-    document.location.replace('/dashboard/');
-  } else {
-    alert(response.statusText);
-  }
-}
+//   if (response.ok) {
+//     document.location.replace('/dashboard/');
+//   } else {
+//     alert(response.statusText);
+//   }
+// }
 
 async function logout() {
   const response = await fetch("/api/users/logout", {
